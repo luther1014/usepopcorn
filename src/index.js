a2c312ba804b289adedac96530f8e23e6801d16d
Luther <luther1096@gmail.com>
2025-01-02 00:58:04 +0530

    created a star ratings re-usable component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRatings from "./StarRatings";
+
+function Test() {
+  const [rating, setRating] = React.useState(0);
+  function handleSetRatings(rating) {
+    setRating(rating);
+  }
+  return (
+    <div>
+      <StarRatings maxRating={10} onSetRatings={handleSetRatings} />
+      <p>This movie was rated {rating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRatings
+      maxRating={5}
+      messages={["Terrible", "Bad", "Average", "Good", "Excellent"]}
+    />
+    <StarRatings maxRating={10} color="red" size={36} defaultRating={7} />
+    <StarRatings />
+    <Test />
   </React.StrictMode>
 );
